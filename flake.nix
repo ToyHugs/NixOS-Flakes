de9702027bf2b04f3ad62ca6664c617a25ba7c0f
toyhugs <pierre.guyot@toyhugs.fr>
2024-12-10 10:46:03 +0100

    chore: Add initial NixOS configuration files

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A ToyHugs Flake";
+
+  # Les entrées sont les dépôts que nous voulons utiliser pour construire notre système
+  inputs = {
+
+    # Nixpkgs stable, pour presque tout les paquets de mon système
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    # Nixpkgs unstable, pour certaines applications nécessitant des versions plus récentes
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home-manager, pour gérer la configuration de mon système, comme les thèmes, les raccourcis clavier, etc.
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs"; # Pour utiliser la même version de nixpkgs
+                                          # et éviter un conflit de version ou doublon
+    };
+
+  };
+
+  # Les sorties sont les dérivations que nous voulons construire
+  outputs = { self, nixpkgs, ... } @ inputs: 
+  let
+
+    # Variable de raccourci
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
+
+  in 
+  {
+
+    # Configuration de mon système
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+    packages.x86_64-linux.hello = pkgs.hello;
+    packages.x86_64-linux.default = pkgs.hello;
+
+  };
+}
